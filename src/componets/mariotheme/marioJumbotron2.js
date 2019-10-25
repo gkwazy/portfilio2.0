db5c40a27cc28c11f949d3cac13caaf1e5af6895
Garret Wasden <gkqazy@gmail.com>
2019-10-25 12:21:32 -0600

    more typos

--- src/componets/mariotheme/marioJumbotron2.js
+++ src/componets/mariotheme/marioJumbotron2.js
@@ -6,7 +6,7 @@ class MarioJumbo2 extends Component {
             <div className="row">
                 <div className="jumbotron jumbotron text-center jumboTwo">
                     <div className="col-8 offset-2">
-                        <h3> I am a full stack developer that has a passion for making people’s web pages look and work to perfection. I have worked with MySQL, Javascript, CSS, HTML, Angularjs, Filemaker and React. I love to learn about new tech that is always coming out and enjoy working in a team to accomplish goals and make a great applications.
+                        <h3> I am a full stack developer that has a passion for making people’s web pages look and work to perfection. I have worked with MySQL, Javascript, CSS, HTML, Angularjs, Filemaker and React. I love to learn about new tech that is always coming out and enjoy working in a team to accomplish goals and make great applications.
             </h3>
                     </div>
                 </div>
